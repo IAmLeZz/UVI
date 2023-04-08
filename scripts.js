0daf195dc23c2bd9224ad6e8772bfa53d4ce1fc0
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-04-08 04:32:13 -0300

    Added project files

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/*
+// Smooth scrolling to section
+$('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+  
+    $('html, body').animate(
+      {
+        scrollTop: $($(this).attr('href')).offset().top,
+      },
+      500,
+      'linear'
+    );
+});*/
