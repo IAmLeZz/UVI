8c245814e36f530707459568306f7403a732899e
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-04-11 02:03:58 -0300

    Add "our team" section. Add some scripts.
    Change some styles.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,28 @@
-var _a;
 $(function () {
-    $(window).on('scroll', function () {
-        var sectionPos = $('#contact').offset().top;
+    $(window).on("scroll", function () {
+        var sectionPos = $("#contact").offset().top;
         var windowHeight = $(window).height();
         var topOfWindow = $(window).scrollTop();
         if (topOfWindow + windowHeight > sectionPos) {
-            $('#contact').addClass('appear');
+            $("#contact").addClass("appear");
         }
     });
 });
 $(function () {
-    $('#big-logo').addClass('appear');
+    $("#big-logo").addClass("appear");
 });
-
-if (window.innerWidth < 800) {
-    (_a = document.querySelector('#buttons')) === null || _a === void 0 ? void 0 : _a.classList.remove('px-5');
+function handleWindowResize() {
+    var _a, _b, _c, _d;
+    if (window.innerWidth < 800) {
+        (_a = document.querySelector("#buttons")) === null || _a === void 0 ? void 0 : _a.classList.remove("px-5");
+        (_b = document.querySelector("#buttons")) === null || _b === void 0 ? void 0 : _b.style.textAlign = "center";
+    }
+    else {
+        (_c = document.querySelector("#buttons")) === null || _c === void 0 ? void 0 : _c.classList.add("px-5");
+        (_d = document.querySelector("#buttons")) === null || _d === void 0 ? void 0 : _d.style.textAlign = "left";
+    }
 }
+// Call the function once to set the initial state
+handleWindowResize();
+// Add event listener for the window resize event
+window.addEventListener("resize", handleWindowResize);
--- scripts.ts
+++ scripts.ts
@@ -1,18 +1,32 @@
-  $(function(){
-    $(window).on('scroll', function(){
-      var sectionPos = $('#contact').offset().top;
-      var windowHeight = $(window).height();
-      var topOfWindow = $(window).scrollTop();
-      if (topOfWindow + windowHeight > sectionPos) {
-        $('#contact').addClass('appear');
-      }
-    });
+$(function () {
+  $(window).on("scroll", function () {
+    var sectionPos = $("#contact").offset().top;
+    var windowHeight = $(window).height();
+    var topOfWindow = $(window).scrollTop();
+    if (topOfWindow + windowHeight > sectionPos) {
+      $("#contact").addClass("appear");
+    }
   });
-  
-  $(function() {
-    $('#big-logo').addClass('appear');
-  });
+});
 
-  if (window.innerWidth < 800){
-    document.querySelector('#buttons')?.classList.remove('px-5');
+$(function () {
+  $("#big-logo").addClass("appear");
+});
+
+function handleWindowResize() {
+  if (window.innerWidth < 800) {
+    document.querySelector("#buttons")?.classList.remove("px-5");
+    document.querySelector("#buttons")?.style.textAlign = "center";
+  } else {
+    document.querySelector("#buttons")?.classList.add("px-5");
+    document.querySelector("#buttons")?.style.textAlign = "left";
   }
+}
+
+// Call the function once to set the initial state
+handleWindowResize();
+
+// Add event listener for the window resize event
+window.addEventListener("resize", handleWindowResize);
+
+
